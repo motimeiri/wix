b441461f2f67bead2247d2fdf6232bc724549a3b
motimeiri <moti_meiri@yahoo.com>
2014-11-10 14:27:38 +0200

    Good luck!

---
+++ Wix/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wix.MvcApplication" Language="C#" %>
---
+++ Wix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wix.Startup))]
+namespace Wix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
